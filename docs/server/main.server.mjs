3e4e65e529891cee5c2b9bad23e11e5b59c6591e
agustin51 <agustin@alpogo.com>
2025-11-16 16:50:54 -0300

    deploy

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-TTR2EO65.mjs";import"./chunk-PKIGNA2Y.mjs";import"./chunk-VTH7QO62.mjs";import"./chunk-CHSKKUCZ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
